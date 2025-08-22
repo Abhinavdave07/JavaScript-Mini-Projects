516b1e14382d5636cfba67e841fc5cde99abce98
Abhinav Dave <mr.abhidave@gmail.com>
2025-08-22 09:47:48 +0530

    to do app i=using dom

---
+++ ToDoUsingDOM/todo.js
@@ -0,0 +1,28 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+    if(inp.value.trim() === "")
+    {
+        alert("Fields cannot be empty");
+    }
+let item = document.createElement("li");
+item.innerText = inp.value;
+ul.appendChild(item);
+
+let delbtn = document.createElement("button");
+delbtn.innerText = "Delete";
+delbtn.classList.add("Delete");
+item.appendChild(delbtn);
+inp.value="";
+});
+
+ul.addEventListener("click",function(event)
+{
+if(event.target.nodeName=="BUTTON")
+{
+    let listitem = event.target.parentElement;
+    listitem.remove();
+}
+});
